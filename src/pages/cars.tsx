f5b6472b295e29181e719bbdec9ce32072a06f39
Tran Thu Trang <tranthutrang@Trans-MacBook-Pro.local>
2020-07-14 21:14:46 +0700

    pagination component

--- src/pages/cars.tsx
+++ src/pages/cars.tsx
@@ -1,21 +1,22 @@
-import { Grid } from "@material-ui/core";
-import Search from ".";
-import { GetServerSideProps } from "next";
-import { getMakes, Make } from "../database/getMakes";
-import { getModels, Model } from "../database/getModel";
-import { getAsString } from "../getAsString";
-import { CarModel } from "../../api/Car";
-import { getPaginatedCars } from "../database/getPaginatedCars";
-import Pagination from "@material-ui/lab/Pagination";
-import PaginationItem from "@material-ui/lab/PaginationItem";
-import { PaginationRenderItemParams } from "@material-ui/lab";
-import { useRouter } from "next/router";
-import Link from "next/link";
-import { ParsedUrlQuery, stringify } from "querystring";
-import { useState } from "react";
-import { forwardRef } from "react";
-import useSWR from "swr";
-import deepEqual from "fast-deep-equal";
+import { Grid } from '@material-ui/core';
+import Search from '.';
+import { GetServerSideProps } from 'next';
+import { getMakes, Make } from '../database/getMakes';
+import { getModels, Model } from '../database/getModel';
+import { getAsString } from '../getAsString';
+import { CarModel } from '../../api/Car';
+import { getPaginatedCars } from '../database/getPaginatedCars';
+import Pagination from '@material-ui/lab/Pagination';
+import PaginationItem from '@material-ui/lab/PaginationItem';
+import { PaginationRenderItemParams } from '@material-ui/lab';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import { ParsedUrlQuery, stringify } from 'querystring';
+import { useState } from 'react';
+import { forwardRef } from 'react';
+import useSWR from 'swr';
+import deepEqual from 'fast-deep-equal';
+import { CarPagination } from '../components/CarPagination';
 
 export interface CarListProps {
   makes: Make[];
@@ -28,41 +29,30 @@ export default function CarsList({
   makes,
   models,
   cars,
-  totalPages
+  totalPages,
 }: CarListProps) {
   const { query } = useRouter();
   const [serverQuery] = useState(query);
 
-  const { data } = useSWR("/api/cars?" + stringify(query), {
+  const { data } = useSWR('/api/cars?' + stringify(query), {
     dedupingInterval: 15000,
     initialData: deepEqual(query, serverQuery)
       ? { cars, totalPages }
-      : undefined
+      : undefined,
     /* if query and serverQuery are the same, pass {cars, totalPages}, if they are different, that means we're alreay on second, third... navigation => ko pass initialData nữa. On first load, current query in browser and query executed in server are the same, but when we go to next page, pass initalData as undefined, so when swr sees no initialData, it will do the call */
   });
-  console.log("data", data);
+  console.log('data', data);
   return (
     <Grid container spacing={3}>
       <Grid item xs={12} sm={5} md={3} lg={2}>
         <Search singleColumn makes={makes} models={models} />
       </Grid>
       <Grid item xs={12} sm={7} md={9} lg={10}>
-        <pre style={{ fontSize: "2.5rem" }}>
-          <Pagination
-            page={parseInt(getAsString(query.page) || "1")}
-            count={totalPages}
-            renderItem={item => (
-              <PaginationItem
-                //component={Link} //Link from Nextjs doesn't work here so we will create our own component
-                component={MULink}
-                query={query} //props tự pass thêm vào
-                item={item} //item={item}
-                {...item}
-              />
-            )}
-          />
+        <CarPagination totalPages={totalPages} />
+        <pre style={{ fontSize: '2.5rem' }}>
           {JSON.stringify(data, null, 4)}
         </pre>
+        <CarPagination totalPages={totalPages} />
       </Grid>
     </Grid>
   );
@@ -76,7 +66,7 @@ export interface MULinkProps {
 const MULink = forwardRef<HTMLAnchorElement, MULinkProps>(
   ({ item, query, ...props }, ref) => (
     <Link
-      href={{ pathname: "/cars", query: { ...query, page: item.page } }}
+      href={{ pathname: '/cars', query: { ...query, page: item.page } }}
       shallow
     >
       <a ref={ref} {...props}></a>
@@ -84,13 +74,15 @@ const MULink = forwardRef<HTMLAnchorElement, MULinkProps>(
   )
 );
 
-export const getServerSideProps: GetServerSideProps<CarListProps> = async ctx => {
+export const getServerSideProps: GetServerSideProps<CarListProps> = async (
+  ctx
+) => {
   const make = getAsString(ctx.query.make);
   //dùng Promise thay vì get từng cái khi muốn get 2 cái cùng lúc chứ ko phải chờ cái nọ xong mới get cái kia
   const [makes, models, pagination] = await Promise.all([
     getMakes(),
     getModels(make),
-    getPaginatedCars(ctx.query)
+    getPaginatedCars(ctx.query),
   ]);
 
   //const makes = await getMakes();
@@ -100,7 +92,7 @@ export const getServerSideProps: GetServerSideProps<CarListProps> = async ctx =>
       makes,
       models,
       cars: pagination.cars,
-      totalPages: pagination.totalPages
-    }
+      totalPages: pagination.totalPages,
+    },
   };
 };
